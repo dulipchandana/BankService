139a527ef3705b9f95944bccf101109badfac0f1
dulip chandana <dcdulip@gamil.com>
2021-10-20 11:50:42 +0530

    BUG-03 fix:add exception calss

--- AccountService/AccountCoreService/src/main/java/com/igreendata/account/service/AccountServiceImpl.java
+++ AccountService/AccountCoreService/src/main/java/com/igreendata/account/service/AccountServiceImpl.java
@@ -1,11 +1,15 @@
 package com.igreendata.account.service;
 
 import com.igreendata.account.dto.AccountDto;
+import com.igreendata.account.exception.IncorrectParameterException;
 import com.igreendata.account.exception.ResourceNotFoundException;
 import com.igreendata.account.model.Account;
 import com.igreendata.account.repository.AccountRepository;
+import org.hibernate.HibernateException;
+import org.hibernate.QueryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -22,11 +26,16 @@ public class AccountServiceImpl implements BankService<AccountDto>{
     @Override
     public Page<AccountDto> getDtoById(Long id, Pageable pageable) {
 
-        Page<AccountDto> accountDtoResults = accountRepository.findAccountByUserId(id,pageable);
-        if(!accountDtoResults.isEmpty()){
-            return accountDtoResults;
-        }else{
-            throw  new ResourceNotFoundException("Account", "userId", id);
+        try {
+            Page<AccountDto> accountDtoResults = accountRepository.findAccountByUserId(id, pageable);
+            if (!accountDtoResults.isEmpty()) {
+                return accountDtoResults;
+            } else {
+                throw new ResourceNotFoundException("Account", "userId", id);
+            }
+        }catch (InvalidDataAccessApiUsageException invalidDataAccessApiUsageException){
+            throw new IncorrectParameterException("Account", "Sort", pageable.getSort());
+
         }
     }
 }
